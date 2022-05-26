76b9a4bfecf279b7d25f55e2ee23ee85ce9a2598
Admin <admin@Admins-iMac.local>
2022-05-26 18:10:40 +0700

    commit at 26/05/2022 18:10:40

--- zap/full_scan.sh
+++ zap/full_scan.sh
@@ -1,3 +1,1 @@
-#docker run -v $(pwd):/zap/wrk/:rw --link web -t owasp/zap2docker-stable zap-full-scan.py -t http://web -w rp.md
-
-echo "Trung" > out.md
+docker run -v $(pwd):/zap/wrk/:rw --link web -t owasp/zap2docker-stable zap-full-scan.py -t http://web -w rp.md
