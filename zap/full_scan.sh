5aeae8cfc3c192cddeb832f4efefef6fd4b926ad
Admin <admin@Admins-iMac.local>
2022-05-26 18:01:12 +0700

    commit at 26/05/2022 18:01:11

--- zap/full_scan.sh
+++ zap/full_scan.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/zap/wrk/:rw --link web -t owasp/zap2docker-stable zap-full-scan.py -t http://web -J report.json
+docker run -v $(pwd):/zap/wrk/:rw --link web -t owasp/zap2docker-stable zap-full-scan.py -t http://web -w rp.md
