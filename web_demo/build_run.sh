5981e00559bb29a1415c22db365856afc03dceef
Admin <admin@Admins-iMac.local>
2022-05-26 15:58:05 +0700

    commit at 26/05/2022 15:58:05

--- web_demo/build_run.sh
+++ web_demo/build_run.sh
@@ -1,3 +1,8 @@
-docker rm web || echo "No find container /web"
-docker build web_demo -t web_demo
+# docker rm web || echo "No find container /web"
+# docker build web_demo -t web_demo
+# docker run -d --rm --name web web_demo
+
+docker ps
+docker rm web
+docker ps
 docker run -d --rm --name web web_demo
