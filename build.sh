a979b993c881382c3a33b8cf0f7212c23384b530
Admin <admin@Admins-MacBook-Pro.local>
2022-06-03 17:26:27 +0700

    dev

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose --file web/docker-compose.yml up -d web nginx
+docker-compose --file web/docker-compose.yml up -d web nginx database
--- web/app_flask/challenge/application/db.py
+++ web/app_flask/challenge/application/db.py
@@ -1,9 +1,41 @@
-import mysql.connector, os
+import mysql.connector, os, token
 
-mydb = mysql.connector.connect(
+con = mysql.connector.connect(
   host="database",
   user="root",
   password=os.getenv('passwdMysql'),
   database='data'
 )
+cursor = con.cursor()
 
+
+def addComment(com):
+  try:
+    cursor.execute("INSERT INTO comments VALUES ('{}')".format(com))
+    con.commit()
+    return True
+  except:
+    return False
+  
+def getComments():
+  cursor.execute("SELECT * FROM comments")
+
+  data = [row[0] for row in cursor.fetchall()]
+  if len(data) > 5:
+    cursor.execute("Delete from comments")
+    con.commit()
+  return data
+
+
+def login(username, password):
+  sql = f"""select username from users where username='{username}' and password='{password}' """
+  try:
+    result = cursor.execute(sql).fetchone()[0]
+    return result
+  except:
+    return None
+  
+  
+  
+  
+  
