ee81cfcb7b7ee82c7f460a611cb85874c7252c7d
vltthle <vltthle@mediba.jp>
2022-06-21 20:08:30 +0700

    [Update] 003_lfi

---
+++ training/003_lfi/lfi/src/db.php
@@ -0,0 +1,9 @@
+<?php
+    $servername = "database";
+    $username = "root";
+    $password = "root";
+    $dbname = "data";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
